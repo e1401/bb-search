4957f6fe3db8a5859f1a6ff5f995dca83eba9f0d
Antonio Hadrović <hadrovic1@gmail.com>
2022-01-09 13:51:33 +0100

    Refactor using logic inside render @CharacterGrid component

--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -3,19 +3,17 @@ import CharacterItem from './CharacterItem';
 
 const CharacterGrid = ({ items, isLoading }) => {
   // console.log(items);
-  return (
-    <div>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <section className="cards">
-          {items.map((item) => (
-            <CharacterItem item={item} key={item.char_id} />
-          ))}
-        </section>
-      )}
-    </div>
+  const displayContent = isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <CharacterItem item={item} key={item.char_id} />
+      ))}
+    </section>
   );
+
+  return <div>{displayContent}</div>;
 };
 
 export default CharacterGrid;
